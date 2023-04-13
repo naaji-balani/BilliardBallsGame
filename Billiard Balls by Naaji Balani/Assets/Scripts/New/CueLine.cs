5e29a7f37e13dd795b2e6cc484254ab2b23b0fc9
developer-techventions <112747410+developer-techventions@users.noreply.github.com>
2023-04-13 21:06:16 +0530

    Update

--- Billiard Balls by Naaji Balani/Assets/Scripts/New/CueLine.cs
+++ Billiard Balls by Naaji Balani/Assets/Scripts/New/CueLine.cs
@@ -6,23 +6,21 @@ using UnityEngine.UI;
 public class CueLine : MonoBehaviour
 {
     private Vector3 lastMousePosition;
-    [SerializeField] Transform _cueAnchor,_cue;
+    [SerializeField] Transform _cueAnchor, _cue;
     public Color c1 = Color.yellow;
     public Color c2 = Color.red;
-    public int lengthOfLineRenderer = 4;
     [SerializeField] LineRenderer lineRenderer;
-    [SerializeField] Camera _topViewCamera,_3dCamera;
+    [SerializeField] Camera _topViewCamera, _3dCamera;
     [SerializeField] Rigidbody[] _allBalls;
     float _forceValue;
     [SerializeField] Slider _forceSlider;
-    [SerializeField] bool _isSliderPressed,_isDragging;
+    [SerializeField] bool _isSliderPressed, _isMoving, _hasPlayed;
 
     void Start()
     {
         _3dCamera.enabled = false;
 
         lineRenderer.widthMultiplier = 0.1f;
-        lineRenderer.positionCount = lengthOfLineRenderer;
 
         // A simple 2 color gradient with a fixed alpha of 1.0f.
         float alpha = 1.0f;
@@ -47,9 +45,13 @@ public class CueLine : MonoBehaviour
 
     void Update()
     {
+        _cueAnchor.rotation = default;
+
+        _cueAnchor.gameObject.SetActive(AllBallsStopped());
+        lineRenderer.enabled = AllBallsStopped();
+
         if (Input.GetMouseButton(0) && !_isSliderPressed && AllBallsStopped())
         {
-            _isDragging = true;
             // Get the horizontal movement of the mouse drag
             float mouseX = Input.mousePosition.x;
             float deltaMouseX = mouseX - lastMousePosition.x;
@@ -62,7 +64,6 @@ public class CueLine : MonoBehaviour
         {
             // Store the last mouse position for the next drag event
             lastMousePosition = Input.mousePosition;
-            _isDragging = false;
         }
 
         if (Input.GetKeyDown(KeyCode.C))
@@ -73,35 +74,27 @@ public class CueLine : MonoBehaviour
 
         Ray a = new Ray(transform.position, transform.forward);
         Ray b;
+        Ray c;
         RaycastHit hit;
 
-        if (Deflect(a, out b, out hit))
+        if (Deflect(a, out b, out c, out hit))
         {
             lineRenderer.SetPosition(0, a.origin);
             lineRenderer.SetPosition(1, hit.point);
 
-            if (hit.collider.CompareTag("Player"))
+            if (hit.collider.CompareTag("Player")) //Other Ball
             {
                 lineRenderer.SetPosition(2, (b.origin));
                 lineRenderer.SetPosition(3, b.origin + 3 * b.direction);
+
+                lineRenderer.SetPosition(4, hit.point);
+                lineRenderer.SetPosition(5, hit.point + 3 * c.direction);
             }
-            else
+            else //Wall
             {
                 lineRenderer.SetPosition(2, hit.point);
                 lineRenderer.SetPosition(3, hit.point + 3 * a.direction);
             }
-        }
-
-        if (AllBallsStopped())
-        {
-            lineRenderer.enabled = true;
-            _cueAnchor.gameObject.SetActive(true);
-
-        }
-        else
-        {
-            lineRenderer.enabled = false;
-            _cueAnchor.gameObject.SetActive(false);
         }
     }
 
@@ -123,39 +116,41 @@ public class CueLine : MonoBehaviour
 
     IEnumerator SliderRecoil(float forceValue)
     {
-        while(_forceSlider.value != 0)
+        while (_forceSlider.value != 0)
         {
             _forceSlider.value -= 5 * Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
         }
 
-        _cueAnchor.gameObject.SetActive(false);
-
         GetComponent<Rigidbody>().AddForce(transform.forward * _forceValue);
+        _cueAnchor.gameObject.SetActive(false);
 
     }
 
-    bool Deflect(Ray ray, out Ray deflected, out RaycastHit hit)
+    bool Deflect(Ray ray, out Ray deflectedTarget, out Ray deflectedCue, out RaycastHit hit)
     {
         if (Physics.Raycast(ray, out hit))
         {
             if (hit.collider.CompareTag("Player"))
             {
                 Vector3 normal = hit.normal;
-                Vector3 deflect = Vector3.Reflect(ray.direction, normal);
+                Vector3 deflectTarget = Vector3.Reflect(ray.direction, normal);
+                Vector3 deflectCue = Vector3.Reflect(deflectTarget, normal);
 
-                deflected = new Ray(hit.point, deflect);
+                deflectedTarget = new Ray(hit.point, deflectTarget);
+                deflectedCue = new Ray(hit.point, deflectCue);
                 return true;
             }
             else
             {
-                deflected = new Ray(hit.point, Vector3.zero);
+                deflectedTarget = new Ray(hit.point, Vector3.zero);
+                deflectedCue = new Ray(hit.point, Vector3.zero);
                 return true;
             }
         }
 
-        deflected = new Ray(Vector3.zero, Vector3.zero);
+        deflectedTarget = new Ray(Vector3.zero, Vector3.zero);
+        deflectedCue = new Ray(Vector3.zero, Vector3.zero);
         return false;
     }
-
 }
